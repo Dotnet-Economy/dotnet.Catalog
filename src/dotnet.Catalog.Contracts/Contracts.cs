0d6c7da5d06adca5764a4fb08fcf7c2a14262ead
Tamara Adokeme <digidonado@yahoo.com>
2022-10-26 13:33:58 -0700

    Added idempotency, newer frontend experience

--- src/dotnet.Catalog.Contracts/Contracts.cs
+++ src/dotnet.Catalog.Contracts/Contracts.cs
@@ -2,8 +2,8 @@ using System;
 
 namespace dotnet.Catalog.Contracts
 {
-    public record CatalogItemCreated(Guid ItemId, string Name, string Description);
-    public record CatalogItemUpdated(Guid ItemId, string Name, string Description);
+    public record CatalogItemCreated(Guid ItemId, string Name, string Description, decimal Price);
+    public record CatalogItemUpdated(Guid ItemId, string Name, string Description, decimal Price);
     public record CatalogItemDeleted(Guid ItemId);
 
 }
